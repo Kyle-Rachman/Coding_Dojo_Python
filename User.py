e7c444fe56ac213591529426d5c8d11fdaa40ec9
Kyle-Rachman <127147261+Kyle-Rachman@users.noreply.github.com>
2023-05-01 16:12:29 -0700

    Add files via upload

--- User.py
+++ User.py
@@ -14,6 +14,7 @@ class User:
         print(f"Age: {self.age}")
         print(f"Rewards Member: {self.is_rewards_member}")
         print(f"Gold Card Points: {self.gold_card_points}")
+        return self
 
     def enroll(self):
         if self.is_rewards_member is not True:
@@ -21,13 +22,19 @@ class User:
             self.gold_card_points = 200
         else:
             print("User already a member")
+        return self
 
     def spend_points(self,amount):
         if amount < self.gold_card_points:
             self.gold_card_points -= amount
         else:
             print("You don't have enough points to do that!")
+        return self
 
 user1 = User("Kyle","Rachman","kcrachman@gmail.com",23)
 user2 = User("George","Washington","none@gmail.com",67)
 user3 = User("Mark", "King", "mking8@gmail.com", 22)
+
+user1.display_info().enroll().spend_points(50).display_info().enroll()
+user2.enroll().spend_points(80).display_info()
+user3.display_info().spend_points(40)
