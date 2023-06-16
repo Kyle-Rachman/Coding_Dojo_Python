ac94892f7b5eb37aa53a4f3541edf817b282aa37
Kyle-Rachman <127147261+Kyle-Rachman@users.noreply.github.com>
2023-06-16 16:59:56 -0700

    Add files via upload

--- login_and_registration/flask_app/controllers/users.py
+++ login_and_registration/flask_app/controllers/users.py
@@ -4,54 +4,49 @@ from flask_app.models.user import User
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 
+# CREATE - Controller Users
+
+@app.route("/register", methods=['POST'])
+def register():
+
+    data = request.form.copy()
+
+    if not User.validate_user(data):
+        return redirect("/")
+    
+    data["pword"] = bcrypt.generate_password_hash(data["pword"])
+    
+    user_id = User.save_user(data)
+    session["user_id"] = user_id
+
+    return redirect("/recipes")
+
+# READ - Controller Users
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/dashboard")
-def dashboard():
-    if not session:
-        return redirect("/")
-    user = User.get_one(session["user_id"])
-    return render_template("dashboard.html", user = user)
-
-@app.route("/register", methods=['POST'])
-def register():
-    data = {
-        "fname" : request.form["first_name"],
-        "lname" : request.form["last_name"],
-        "email" : request.form["email"],
-        "pword" : request.form["password"],
-        "confirm" : request.form["confirm"]
-    }
-    if not User.validate_user(data):
-        return redirect("/")
-    data["pword"] = bcrypt.generate_password_hash(data["pword"])
-    user_id = User.save(data)
-    session["user_id"] = user_id
-
-    return redirect("/dashboard")
-
 @app.route("/login", methods=['POST'])
 def login():
-    data = {
-        "email" : request.form["email"]
-    }
 
-    user_in_db = User.get_by_email(data)
+    data = request.form
+
+    user_in_db = User.get_user_by_email(data)
+    
     if not user_in_db:
-        flash("Invalid email", "login")
+        flash("Incorrect email/password!", "login")
         return redirect("/")
     
-    if not bcrypt.check_password_hash(user_in_db.password,request.form["password"]):
-        flash("Incorrect password!", "login")
+    if not bcrypt.check_password_hash(user_in_db.password, data["password"]):
+        flash("Incorrect email/password!", "login")
         return redirect("/")
 
     session["user_id"] = user_in_db.id
 
-    return redirect("/dashboard")
+    return redirect("/recipes")
 
-@app.route("/logout", methods=['POST'])
+@app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
--- login_and_registration/flask_app/models/user.py
+++ login_and_registration/flask_app/models/user.py
@@ -2,6 +2,8 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import app
 from flask import flash
 import re
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 class User:
     def __init__(self, data):
@@ -12,27 +14,77 @@ class User:
         self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        self.recipes = []
+
+    # CREATE - MODELS
+
+    @classmethod
+    def save_user(cls, data):
+        query = "INSERT INTO users(first_name, last_name, email, password) VALUES (%(fname)s, %(lname)s, %(email)s, %(pword)s)"
+        user_id = connectToMySQL('recipes_schema').query_db(query, data)
+        return user_id
+
+    # READ - MODELS
 
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL('logins_schema').query_db(query)
+        results = connectToMySQL('recipes_schema').query_db(query)
         users = []
         for user in results:
             users.append(cls(user))
         return users
+    
+    @classmethod
+    def get_all_emails(cls):
+        query = "SELECT email FROM users;"
+        results = connectToMySQL('recipes_schema').query_db(query)
+        users = []
+        for user in results:
+            users.append(user["email"])
+        return users
 
     @classmethod
-    def get_one(cls, id):
+    def get_user_by_id(cls, id):
         query = "SELECT * FROM users WHERE id = %(id)s;"
         data = {"id" : id}
-        results = connectToMySQL('logins_schema').query_db(query, data)
+        results = connectToMySQL('recipes_schema').query_db(query, data)
         return cls(results[0])
 
     @classmethod
-    def save(cls, data):
-        query = "INSERT INTO users(first_name, last_name, email, password, created_at, updated_at) VALUES (%(fname)s, %(lname)s, %(email)s, %(pword)s, NOW(), NOW())"
-        return connectToMySQL('logins_schema').query_db(query, data)
+    def get_user_by_id_with_recipes(cls, id):
+        query = "SELECT * from users LEFT JOIN recipes on recipes.users_id = users.id WHERE users.id = %(id)s=;"
+        data = {"id" : id}
+        results = connectToMySQL('recipes_schema').query_db(query, data)
+        user = cls(results[0])
+
+        if results[0]["description"]:
+            for row_from_db in results:
+                recipe_data = {
+                    "id" : row_from_db["recipes.id"],
+                    "name" : row_from_db["recipes.name"],
+                    "description" : row_from_db["recipes.description"],
+                    "under" : row_from_db["recipes.under"],
+                    "instructions" : row_from_db["recipes.instructions"],
+                    "date_made" : row_from_db["recipes.date_made"],
+                    "created_at" : row_from_db["recipes.created_at"],
+                    "updated_at" : row_from_db["recipes.updated_at"],
+                    "users_id" : row_from_db["recipes.users_id"]
+                }
+
+                user.recipes.append(recipe.Recipe(recipe_data))
+        
+        return user
+
+    @classmethod
+    def get_by_email(cls, data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL('recipes_schema').query_db(query, data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+    
+        # VALIDATIONS
 
     @staticmethod
     def validate_user(user):
@@ -55,11 +107,8 @@ class User:
                 is_valid = False
                 flash("Invalid email!" , "registration")
             else:
-                accounts = User.get_all()
-                account_emails = []
-                for account in accounts:
-                    account_emails.append(account.email)
-                if user["email"] in account_emails:
+                emails = User.get_all_emails()
+                if user["email"] in emails:
                     is_valid = False
                     flash("Duplicate email!", "registration")
         if not user["pword"]:
@@ -70,22 +119,14 @@ class User:
                 is_valid = False
                 flash("Password not long enough!", "registration")
             else:
-                if user["pword"] != user["confirm"]:
+                if user['pword'] != user['confirm']:
                     is_valid = False
                     flash("Passwords don't match", "registration")
                 if user["pword"].islower():
                     is_valid = False
                     flash("Password must contain an uppercase letter", "registration")
-                if not any(char.isdigit() for char in user["pword"]):
+                if not [char for char in user["pword"] if char.isdigit()]:
                     is_valid = False
                     flash("Password must contain a number", "registration")
 
         return is_valid
-
-    @classmethod
-    def get_by_email(cls, data):
-        query = "SELECT * FROM users WHERE email = %(email)s;"
-        results = connectToMySQL('logins_schema').query_db(query, data)
-        if len(results) < 1:
-            return False
-        return cls(results[0])
